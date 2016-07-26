c954ba67fd842375d9b37d63bc42a504bbf8fcbb
ashishraj91 <ashishrajhits@gmail.com>
2016-07-26 12:52:10 +0530

    added db cookbook

---
+++ AMD_DB/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:AMD_DB][:db_package] = ['mariadb-server', 'mariadb']
+default[:AMD_DB][:mysql_dir] = '/etc/my.cnf'
---
+++ AMD_DB/recipes/default.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: AMD_DB
+# Recipe:: default
+#
+# Copyright 2016, Relevance Lab Pvt LTD, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node[:AMD_DB][:db_package].each do |pkg|
+package pkg
+end
+
+include_recipe "mount_fsystem::default"
+
+template node[:AMD_DB][:mysql_dir] do
+	source 'my.cnf.erb'
+end
+
+execute "enable selinux" do
+	command "setenforce 0"
+end
+
+execute "start mariadb" do
+	command "systemctl start mariadb.service"
+end
+
+execute "create database" do
+	command <<-EOF
+	echo "create database analyticsMD" | mysql
+	echo "grant all on analyticsMD.* to 'root'@'%' identified by 'root';" | mysql
+	echo "flush privileges;" | mysql
+	EOF
+	not_if 'mysql -uroot -e "show databases" | grep analyticsMD'
+end
+
+template '/etc/hostname' do
+	source 'hostname.erb'
+end
+
