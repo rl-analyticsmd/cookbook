7ed4e82340332ca331bbd1799b7c6afbc92d292b
ericywho <ericywho@yahoo.com>
2016-09-14 15:55:55 -0700

    more changes

--- zabbix_server/recipes/default.rb
+++ zabbix_server/recipes/default.rb
@@ -13,13 +13,39 @@ end
 
 package 'zabbix-server'
 
-template '/etc/zabbix/zabbix_agentd.conf' do
-	source 'zabbix_agentd.conf.erb'
+#template '/etc/zabbix/zabbix_agentd.conf' do
+#	source 'zabbix_agentd.conf.erb'
+#end
+
+# execute "disable selinux" do
+# 	command "setenforce 0"
+# end
+
+# Per Ian, he said don't disable selinux.  But we need to remove any zabbix module, if it were there before.
+execute "remove any preexisting zabbix module from selinx" do
+	command "semodule -r zabbix"
 end
 
-execute "disable selinux" do
-	command "setenforce 0"
+execute "install mysql for zabbix server" do
+	yum install zabbix-server-mysql zabbix-web-mysql
 end
+
+# Now, configure the mysql db.
+execute "configure mysql db for zabbix server" do
+	cd /usr/share/doc/zabbix-server-mysql-3.0.0
+	zcat create.sql.gz | mysql -uroot zabbix
+end
+
+# Now, setup the zabbix server config file
+template '/etc/zabbix/zabbix_server.conf' do
+       source 'zabbix_server.conf.erb'
+end
+
+# Now, configure zabbix frontend
+template '/etc/httpd/conf.d/zabbix.conf' do
+	source 'zabbix_httpd.conf.erb'
+end
+
 
 service "zabbix-server" do
 supports :status => true, :restart => true, :reload => true
